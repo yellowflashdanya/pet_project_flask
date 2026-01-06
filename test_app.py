4469946372846c596e6f0ef432c668d989399000
Danylo Melnychuk <danyakid2005@gmail.com>
2026-01-06 18:44:50 +0200

    test: add tests for app

--- test_app.py
+++ test_app.py
@@ -4,10 +4,13 @@ from app import app
 @pytest.fixture
 def client():
   app.config['TESTING'] = True
+
   with app.test_client() as client:
     yield client
 
-def test_homepage():
+def test_homepage(client):
   response = client.get('/')
+
   assert response.status_code == 200
-  assert b"I have been seen" in response.data
+
+  assert b"Hello! I see you" in response.data
