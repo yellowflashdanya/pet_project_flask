4cf8b1b82bea5aabb0a30229d9ca82f0d5dad2da
Danylo Melnychuk <danyakid2005@gmail.com>
2026-01-06 21:43:37 +0200

    fix: add entry point for flask app to run in docker

--- app/app.py
+++ app/app.py
@@ -25,3 +25,7 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return f'Hello! I see you {count} times.\n'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
