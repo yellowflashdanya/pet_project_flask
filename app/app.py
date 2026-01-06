42db796891f55614265d6233318220111bcaa55b
Danylo Melnychuk <danyakid2005@gmail.com>
2026-01-06 20:06:08 +0200

    style: fix indentation and PEP8 formatting

--- app/app.py
+++ app/app.py
@@ -6,24 +6,26 @@ import os
 app = Flask(__name__)
 
 if os.environ.get("TESTING") == "True":
-  class FakeRedis:
-    def incr(self, key): return 1
-  cache = FakeRedis()
+    class FakeRedis:
+        def incr(self, key): return 1
+    cache = FakeRedis()
 else:
-  cache = redis.Redis(host='redis', port=6379)
+    cache = redis.Redis(host='redis', port=6379)
+
 
 def get_hit_count():
-  retries = 5
-  while True:
-    try:
-      return cache.incr('hits')
-    except redis.exceptions.ConnectionError as exc:
-      if retries == 0:
-        raise exc
-      retries -= 1
-      time.sleep(0.5)
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
 
 @app.route('/')
 def hello():
-  count = get_hit_count()
-  return f'Hello! I see you {count} times.\n'
+    count = get_hit_count()
+    return f'Hello! I see you {count} times.\n'
--- test_app.py
+++ test_app.py
@@ -1,20 +1,21 @@
 import pytest
 import os
+from app.app import app as flask_app
 
 os.environ["TESTING"] = "True"
 
-from app.app import app as flask_app
 
 @pytest.fixture
 def client():
-  flask_app.config['TESTING'] = True
+    flask_app.config['TESTING'] = True
 
-  with flask_app.test_client() as client:
-    yield client
+    with flask_app.test_client() as client:
+        yield client
+
 
 def test_homepage(client):
-  response = client.get('/')
+    response = client.get('/')
 
-  assert response.status_code == 200
+    assert response.status_code == 200
 
-  assert b"Hello! I see you" in response.data
+    assert b"Hello! I see you" in response.data
