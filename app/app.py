599d435de5f56c38b8e2788b68038c842e0e98e7
Danylo Melnychuk <danyakid2005@gmail.com>
2026-01-18 12:39:05 +0200

    feat: add healthcheck endpoint

--- app/app.py
+++ app/app.py
@@ -27,5 +27,10 @@ def hello():
     return f'Hello! I see you {count} times.\n'
 
 
+@app.route('/health')
+def health():
+    return {"status": "healthy"}, 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
